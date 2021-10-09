bcb6104c25147ae09d513d40f54885b004ed3d16
FaKenKoala <60820399+FaKenKoala@users.noreply.github.com>
2021-10-10 00:22:23 +0800

    Dev (#7)

--- test.dart
+++ test.dart
@@ -1,9 +1,13 @@
-void test(int value){}
+void test(int value) {}
 
-void test2(String params){}
+void test2(String params) {}
 
-void test3(){}
-void test5(){}
+void test3() {}
+void test5() {}
 
-void test6(){}
-void test7(){}
+void test6() {}
+void test7() {}
+
+void test8() {}
+
+void test9() {}
